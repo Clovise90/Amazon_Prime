f81ea0466b1e4777350ddec78837e40179f0fdba
Clovise <cloviselehdogha@gmail.com>
2024-10-08 18:42:06 -0600

    add

---
+++ s3.tf
@@ -0,0 +1,9 @@
+#Branches : gives the posibility to copy and modify code without affecting original
+# master/main are production branch. main branch is alwasys protected 
+
+
+resource "aws_s3_bucket" "bucket2432" {
+    bucket = "bucket2432"
+    
+  
+}
